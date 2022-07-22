fe2c6141ea772cc4cc157533b89857c42d32bb6a
Joaquin Alejandro Rojas <joaquin1709@gmail.com>
2022-07-21 22:26:14 -0300

    primer practico java

---
+++ java.js
@@ -0,0 +1,11 @@
+let numero = parseInt(prompt("ingrese un numero mayor a 10"))
+
+while(numero < 10) {
+    numero = parseInt("ingrese un numero mayor a 10")
+}
+
+for(let i = 2; i <= numero; i++) {
+    console.log(i)
+}
+
+console.log("FIN")
